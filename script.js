2718cffabab986ab62dbbc0a894f3050fd50a68c
Xenon <gg.riptor@gmail.com>
2020-04-18 22:41:59 +0700

    Удалены лишние комментарии

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
- // Функция ymaps.ready() будет вызвана, когда
- // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
- ymaps.ready(init);
- function init(){
- // Создание карты.
- window.lsm = new ymaps.Map("LiveStreets", {
- // Координаты центра карты.
- // Порядок по умолчанию: «широта, долгота».
- // Чтобы не определять координаты центра карты вручную,
- // воспользуйтесь инструментом Определение координат.
- center: [52.84460345, 88.04247036],
- // Уровень масштабирования. Допустимые значения:
- // от 0 (весь мир) до 19.
- zoom: 10,
- nativeFullscreen: true,
- controls: ['zoomControl', 'fullscreenControl', 'typeSelector', 'searchControl'],
- restrictMapArea: [[53.294794700094776,87.06966604079525],[52.439613574668044,89.06917775954112]],
- suppressMapOpenBlock: true
- });
-searchControl = lsm.controls.get('searchControl');
-searchControl.options.set({
-	noPlacemark: true,
-	placeholderContent: 'Введите название улицы',
-	boundedBy: [[53.294794700094776,87.06966604079525],[52.439613574668044,89.06917775954112]],
-	strictBounds: true,
-	suppressYandexSearch: true,
-	noSuggestPanel: true
-	});
- 
-// Значения цветов иконок.
+ymaps.ready(init);
+function init() {
+    window.lsm = new ymaps.Map("LiveStreets", {
+        center: [52.84460345, 88.04247036],
+        zoom: 10,
+        nativeFullscreen: true,
+        controls: ['zoomControl', 'fullscreenControl', 'typeSelector', 'searchControl'],
+        restrictMapArea: [[53.294794700094776, 87.06966604079525], [52.439613574668044, 89.06917775954112]],
+        suppressMapOpenBlock: true
+    });
+    searchControl = lsm.controls.get('searchControl');
+    searchControl.options.set({
+        noPlacemark: true,
+        placeholderContent: 'Введите название улицы',
+        boundedBy: [[53.294794700094776, 87.06966604079525], [52.439613574668044, 89.06917775954112]],
+        strictBounds: true,
+        suppressYandexSearch: true,
+        noSuggestPanel: true
+    });
+
+    // Значения цветов иконок.
     let placemarkColors = [
-            '#DB425A', '#4C4DA2', '#00DEAD', '#D73AD2',
-            '#F8CC4D', '#F88D00', '#AC646C', '#548FB7'
-        ],
+        '#DB425A', '#4C4DA2', '#00DEAD', '#D73AD2',
+        '#F8CC4D', '#F88D00', '#AC646C', '#548FB7'
+    ],
         clusterer = new ymaps.Clusterer({
             // Макет метки кластера pieChart.
             clusterIconLayout: 'default#pieChart',
@@ -43,8 +34,8 @@ searchControl.options.set({
             clusterIconPieChartStrokeWidth: 3,
             // Определяет наличие поля balloon.
             //hasBalloon: false,
-			hasHint: false,
-			//gridSize: 999999
+            hasHint: false,
+            //gridSize: 999999
         }),
         points = [
             [52.76408647, 87.86300161], [52.76461362, 87.86288359], [52.76736671, 87.87067314], [52.75888720, 87.84888825],
@@ -53,36 +44,36 @@ searchControl.options.set({
         geoObjects = [];
 
     for (var i = 0, len = points.length; i < len; i++) {
-        geoObjects[i] = new ymaps.Placemark(points[i], {hintContent: 'Проект *****'}, {
+        geoObjects[i] = new ymaps.Placemark(points[i], { hintContent: 'Проект *****' }, {
             iconColor: getRandomColor()
         });
     }
 
     clusterer.add(geoObjects);
     //lsm.geoObjects.add(clusterer);
-console.log(clusterer.getBounds());
-/* автоматический зум на центр кластера
-    lsm.setBounds(clusterer.getBounds(), {
-        checkZoomRange: false
-    });
-*/
+    console.log(clusterer.getBounds());
+    /* автоматический зум на центр кластера
+        lsm.setBounds(clusterer.getBounds(), {
+            checkZoomRange: false
+        });
+    */
     function getRandomColor() {
         return placemarkColors[Math.floor(Math.random() * placemarkColors.length)];
     }
 
 
 
-/* ---------
-let myPlacemark = new ymaps.Placemark([52.76173973, 87.85590679], {
-    balloonContentHeader: 'Школа №1',
-    balloonContentBody: '<div class="icon"><img src="https://sun9-58.userapi.com/c845420/v845420937/3900b/Z44u3uvkI7I.jpg?ava=1"/></div><p>Описание проекта</p>',
-    balloonContentFooter: '<a href="javascript://">Подробнее</a>',
-    hintContent: 'Проект *****'
-});
-
-lsm.geoObjects.add(myPlacemark);
-
-// Балун откроется в точке «привязки» балуна — т. е. над меткой.
-// myPlacemark.balloon.open();*/
+    /* ---------
+    let myPlacemark = new ymaps.Placemark([52.76173973, 87.85590679], {
+        balloonContentHeader: 'Школа №1',
+        balloonContentBody: '<div class="icon"><img src="https://sun9-58.userapi.com/c845420/v845420937/3900b/Z44u3uvkI7I.jpg?ava=1"/></div><p>Описание проекта</p>',
+        balloonContentFooter: '<a href="javascript://">Подробнее</a>',
+        hintContent: 'Проект *****'
+    });
+    
+    lsm.geoObjects.add(myPlacemark);
+    
+    // Балун откроется в точке «привязки» балуна — т. е. над меткой.
+    // myPlacemark.balloon.open();*/
 
 }
